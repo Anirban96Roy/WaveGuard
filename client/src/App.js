5cb335ffabb65a510452c2d4187799d04753e072
AritraLikhan <aritralikhan2001@gmail.com>
2024-10-27 10:15:37 +0600

    Homepage added

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import { Routes, Route,Navigate } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <>
+     <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/About" element={<About/>} />
+      </Routes>
+    </>
+  );
+}
+
+
+
+export default App;
---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+import '../customCSS/homepage.css';
+import v from '../components/v.mp4';
+const HomePage = () => {
+  return (
+    <Layout>
+            <div className="content">
+                <video autoPlay loop muted playsInline className="bgvideo">
+                    <source src={v} type="video/mp4" />
+                </video>
+                <div className="overlay"></div> {/* Dark overlay on video */}
+                <h1>Flood Management & Monitoring System</h1>
+                <p>
+                    Welcome to our Flood Management & Monitoring System, designed to provide real-time data and resources to help communities prepare for and respond to flooding events.<br />
+                    Our platform utilizes advanced technology to track weather patterns, monitor water levels, and provide timely alerts.<br />
+                    We aim to enhance safety and resilience by empowering individuals and organizations with the information they need to mitigate the impact of floods. Join us in our mission to create safer, more prepared communities!
+                </p>
+                <div className="buttonx">
+                    <button type="button">
+                        Need Help?
+                        <span><span></span> </span> {/* This span is for the hover effect */}
+                    </button>
+                    <button type="button">
+                        Wanna Be a Volunteer?
+                        <span></span> {/* This span is for the hover effect */}
+                    </button>
+                </div>
+
+                <div className="Information">
+                    <div className="Information-row">
+                        <div className="Information-card">
+                        <h3>Information Card</h3>
+                        <p>Lorem ipsum dolor sit amet,  consectetur adipiscing elit</p>
+                        <a>Learn more</a> 
+                        </div>
+                        <div className="Information-card">
+                        <h3>Information Card</h3>
+                        <p>Lorem ipsum dolor sit amet,  consectetur adipiscing elit</p>
+                        <a>Learn more</a> 
+                        </div>
+                        <div className="Information-card">
+                        <h3>Information Card</h3>
+                        <p>Lorem ipsum dolor sit amet,  consectetur adipiscing elit</p>
+                        <a>Learn more</a>     
+                        </div>
+                    </div>
+                </div>
+
+                   
+
+            </div>
+        </Layout>
+  )
+}
+
+export default HomePage
